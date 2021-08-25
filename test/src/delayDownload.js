b71eed3b4890bfe5e502a6b6b7fb9bfc62d7efa3
zhilinx <zhilinx@chinadci.com>
2021-08-25 15:39:58 +0800

    【新增】测试用例

---
+++ test/src/delayDownload.js
@@ -0,0 +1,20 @@
+import docxload from '@src/index'
+
+_.addBtn('延迟下载', done => {
+  let template = `
+    <page>
+      <p>
+        <img width="200" height="150" src="https://tse4-mm.cn.bing.net/th/id/OIP-C.QYq9ucU9cbRRJPQKg24ntAHaEr?w=263&h=180&c=7&o=5&pid=1.7" />
+      </p>
+    </page>
+  `
+
+  docxload(template, { immediate: false })
+    .then(([blob, exportFile]) => {
+      setTimeout(() => {
+        console.log(blob)
+        exportFile(blob, '延迟下载')
+        done()
+      }, 1000)
+    })
+})
--- test/utils/btn/index.js
+++ test/utils/btn/index.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-function addBtn({ name, onClick } = {}) {
+function addBtnDom({ name, onClick } = {}) {
   let container = document.querySelector('#btn-container')
   let btn = document.createElement('div')
   btn.setAttribute('class', 'btn')
@@ -11,6 +11,27 @@ function addBtn({ name, onClick } = {}) {
   onClick && btn.addEventListener('click', onClick)
 
   container.appendChild(btn)
+}
+
+function addBtn(name, fn) {
+  let baseStyle = 'font-weight: bold; background: #4679a7; color: #fff; padding: 4px 6px; border-radius: 6px;'
+
+  addBtnDom({
+    name,
+    onClick() {
+      console.log(`%cbegin: ${name}`, baseStyle)
+
+      const beginTime = Date.now()
+      const handler = () => new Promise(resolve => fn ? fn(resolve) : resolve())
+
+      handler().then(() => {
+        const endTime = Date.now()
+        console.log(`%cend: ${name}%c  [${endTime - beginTime} ms]`, baseStyle, 'color: green;')
+      }).catch(err => {
+        console.error(`failed: ${name}%c`, err)
+      })
+    }
+  })
 }
 
 export default addBtn
