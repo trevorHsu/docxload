df0bfb43ba2afe9a9b7f753450f0b583668db788
zhilinx <zhilinx@chinadci.com>
2021-10-14 10:43:12 +0800

    【修复】在页面方向为horizontal时，表格宽度计算错误，导致宽度显示不正确

--- src/Complier/sectionComponent/table/table-attrs.js
+++ src/Complier/sectionComponent/table/table-attrs.js
@@ -1,11 +1,14 @@
-
 import { WidthType } from 'docx'
-import { PAGE_WIDTH, PCT_REG } from './variables'
+import { PAGE_WIDTH, PAGE_WIDTH_HORIZONTAL, PCT_REG } from './variables'
+import { PAGE_ORIENTATION } from '../section/variables'
 
 // word 2007 宽度配置无法使用百分比，因此用dxa类型来设置宽度
 function width(val, attrs) {
+  const pageWidth = (attrs._pageOrientation_ && attrs._pageOrientation_ === PAGE_ORIENTATION.horizontal)
+    ? PAGE_WIDTH_HORIZONTAL
+    : PAGE_WIDTH
   const widthPercentage = PCT_REG.test(val) ? Number(PCT_REG.exec(val)[1]) : 100
-  const tableWidth = widthPercentage / 100 * PAGE_WIDTH
+  const tableWidth = widthPercentage / 100 * pageWidth
 
   attrs.width = {
     size: tableWidth,
--- src/Complier/sectionComponent/table/variables.js
+++ src/Complier/sectionComponent/table/variables.js
@@ -1,4 +1,5 @@
 const PAGE_WIDTH = 9030
+const PAGE_WIDTH_HORIZONTAL = 13940
 const PCT_REG = /^(([0-9]*[1-9][0-9]*)(\.\d+)?)(%+$)?/
 
-export { PAGE_WIDTH, PCT_REG }
+export { PAGE_WIDTH, PAGE_WIDTH_HORIZONTAL, PCT_REG }
